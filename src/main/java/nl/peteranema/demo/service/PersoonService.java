d5e38d95f4a190a19dbf8c70e7d838bbadb6db92
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-09-14 18:51:32 +0200

    Added interface for PersoonService

--- src/main/java/nl/peteranema/demo/service/PersoonService.java
+++ src/main/java/nl/peteranema/demo/service/PersoonService.java
@@ -1,45 +1,12 @@
 package nl.peteranema.demo.service;
 
-import nl.peteranema.demo.exception.RecordNotFoundException;
 import nl.peteranema.demo.model.Persoon;
-import nl.peteranema.demo.repository.PersoonRepository;
 
-import java.util.Optional;
+public interface PersoonService {
 
-public class PersoonService {
+    Iterable<Persoon> findAll();
+    public Persoon findById(long nr);
+    void save(Persoon persoon);
+    void deleteById(long nr);
 
-    private final PersoonRepository persoonRepository;
-
-    public PersoonService(PersoonRepository persoonRepository) {
-        this.persoonRepository = persoonRepository;
-    }
-
-    public Iterable<Persoon> findAll() {
-        Iterable<Persoon> personen = persoonRepository.findAll();
-        return personen;
-    }
-
-    public Persoon findById(long nr) {
-        Optional<Persoon> persoon = persoonRepository.findById(nr);
-        if (persoon.isPresent()) {
-            return persoon.get();
-        }
-        else {
-            throw new RecordNotFoundException("Persoon with id " + nr + " not found");
-        }
-    }
-
-    public void save(Persoon persoon) {
-        persoonRepository.save(persoon);
-    }
-
-    public void deleteById(long nr) {
-        try {
-            persoonRepository.deleteById(nr);
-        }
-        catch (IndexOutOfBoundsException ex) {
-            System.out.println(ex);
-            throw new RecordNotFoundException();
-        }
-    }
 }
